b5efeb4d242ccd52b8d945b03ab4baf8bc5eec29
ronitmotivaras <ronitmotivaras04@gmail.com>
2025-01-05 23:39:04 +0530

    Added more programmes

--- Patterns/inverted_triangle_pattern_character.cpp
+++ Patterns/inverted_triangle_pattern_character.cpp
@@ -4,19 +4,21 @@ using namespace std;
 int main()
 {
     int n = 4;
+    char ch='A';
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < i; j++)
             cout << " ";
         for (int j = 0; j <n-i; j++)
         {
-            cout << i+1;
+            cout <<ch;
         }
+        ch++;
         cout << endl;
     }
     return 0;
 }
-// 1111
-//  222
-//   33
-//    4
+// AAAA
+//  BBB
+//   CC
+//    D
--- Patterns/reverse_character_triangle_pattern.cpp
+++ Patterns/reverse_character_triangle_pattern.cpp
@@ -3,20 +3,21 @@ using namespace std;
 
 int main()
 {
-    //Reverse Tringle Pattern
+    //Reverse character Tringle Pattern
     int n = 4;
+    char ch = 'A';
     for (int i = 0; i < n; i++)
     { 
         for (int j = i; j >=0; j--)
         { 
-            cout<<j+1<<" ";
+            cout<<ch+n<<" ";
+            ch--;
         }
         cout << endl;
     }
     return 0;
 }
-
-// 1
-// 2 1
-// 3 2 1
-// 4 3 2 1
+// A
+// B A
+// C B A
+// D C B A
